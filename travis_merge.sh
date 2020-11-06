197fc2b05b731390a0c3d8b691f932ac91cc35aa
vegantofuboy <64468897+vegantofuboy@users.noreply.github.com>
2020-11-06 09:36:51 +0100

    Update travis_merge.sh

--- travis_merge.sh
+++ travis_merge.sh
@@ -5,6 +5,6 @@ export GIT_COMMITTER_EMAIL="flonowa@outlook.com"
 export GIT_COMMITTER_NAME="florian"
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
-git checkout master || exit
+git checkout main || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
 git push @github.com/">https://${GITHUB_TOKEN}@github.com/vegantofuboy/ansible_role_baseline.git
